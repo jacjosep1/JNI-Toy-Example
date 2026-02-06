392c1a527caee0ac95dbad14179dbab1338adb06
Joseph Jackson <jacjosep@umich.edu>
2026-02-06 11:11:50 -0500

    added code for links

--- incidence_data_structure/top_down_sorting.py
+++ incidence_data_structure/top_down_sorting.py
@@ -31,20 +31,24 @@ class TopDownSorting:
         # Build map {binned vertices in edge} -> frequency
         insertion_map : map[tuple, int] = defaultdict(int)
         weights_map : map[tuple, float] = defaultdict(float)
+        link_map : map[tuple, list[int]] = defaultdict(list[int]) # Keep track of which raw edges correspond to subedges
         for raw_edge in link:
             # Bin raw edges in this link according to new resolution n
-            cols = np.floor(self.nz_columns(self.raw_data, raw_edge) / n_new).astype(int)
-            insertion_map[tuple(cols)] += 1
-            weights_map[tuple(cols)] += self.raw_data[raw_edge]
+            cols = tuple(np.floor(self.nz_columns(self.raw_data, raw_edge) / n_new).astype(int))
+            insertion_map[cols] += 1
+            weights_map[cols] += self.raw_data[raw_edge]
+            link_map[cols].append(raw_edge)
 
         # Build csr matrix from map with d << k
         d = len(insertion_map)
         sub_csr = csr_matrix((d, n_new))
         sub_W = np.zeros(d)
+        sub_links : list[list[int]] = [[] for _ in range(d)]
 
         for i, (cols, _) in enumerate(insertion_map.items()):
             sub_csr[i, list(cols)] = 1
             sub_W[i] = weights_map[cols]
+            sub_links[i] = sub_links[cols]
 
         # Sort lexicographically by vertex position
         def lex_key(A: csr_matrix, i: int):
@@ -55,34 +59,37 @@ class TopDownSorting:
         order = sorted(range(d), key = lambda i: lex_key(sub_csr, i))
         sub_csr = sub_csr[order]
         sub_W = sub_W[order]
+        sub_links = sub_links[order]
 
-        return sub_csr, sub_W # TODO : find and return links
+        return sub_csr, sub_W, sub_links
 
 
 
-    def sort_at_resolution(self, prev_H : csr_matrix, prev_links : list[list[int]]):
+    def sort_at_resolution(self, m : int, n : int, prev_links : list[list[int]]):
         '''
         Main function for computing H & W at each resolution given the previous smaller one. 
 
-        prev_H: m x n binned incidence matrix from previous iteration
+        m: previous # edges
+        n: previous resolution
         prev_links: list of size m, maps to all raw edges aggregated into each row of prev_H
                     In the actual program, these will point to locations to a raw datafile on disk,
                     or uniqueptrs if we can spare the memory to load all of a SPRITE instance.  
 
         returns: higher resolution incidence H, edge weights W, and new links
         '''
-        m, n = prev_H.shape
         n_new = n * 2
 
         stack_H = []
         stack_W = []
+        combined_links = []
         for prev_edge in range(m):
             link = prev_links[prev_edge]
-            sub_H, sub_W = self.expand_edge(link, n_new)
+            sub_H, sub_W, sub_links = self.expand_edge(link, n_new)
             stack_H.append(sub_H)
             stack_W.append(sub_W)
+            combined_links.extend(sub_links)
 
         combined_H = sp.vstack(stack_H)
         combined_W = np.hstack(stack_W)
 
-        return combined_H, combined_W
+        return combined_H, combined_W, combined_links
